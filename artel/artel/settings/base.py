d9ee2c8c812393fda32b9b0e0d37d0fd32cb9c1c
mtyton <mateusz.tyton99@gmail.com>
2023-04-29 11:51:04 +0200

    added blog section

--- artel/artel/settings/base.py
+++ artel/artel/settings/base.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     "home",
     "store",
+    "blog",
     "search",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     "wagtail.admin",
     'wagtail.contrib.modeladmin',
     "wagtail",
+    "wagtailmenus",
     "modelcluster",
     "taggit",
     "django.contrib.admin",
@@ -75,6 +77,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
