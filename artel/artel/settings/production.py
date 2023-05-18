4a299071d20c17839e9b4744f363d2573445e3d1
mtyton <mateusz.tyton99@gmail.com>
2023-05-18 19:48:03 +0200

    updated allowed hosts

--- artel/artel/settings/production.py
+++ artel/artel/settings/production.py
@@ -4,7 +4,8 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = [
-    "91.195.93.3",
+    "http://91.195.93.3",
+    "https://91.195.93.3",
     "localhost",
     "0.0.0.0",
     "127.0.0.1"
