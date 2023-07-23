dcae7a433fd7bace211fba15d9ce12128ed921a7
mtyton <mateusz.tyton99@gmail.com>
2023-07-23 19:16:08 +0200

    updated mail context

--- artel/store/tasks.py
+++ artel/store/tasks.py
@@ -3,6 +3,7 @@ from django.conf import settings
 
 from mailings.models import OutgoingEmail
 from store.models import Product
+from store.admin import ProductAdmin
 
 
 logger = logging.getLogger(__name__)
@@ -16,11 +17,12 @@ def send_produt_request_email(variant_pk: int):
         logger.exception(f"Product with pk={variant_pk} does not exist")
 
     try:
+        admin_url = ProductAdmin().url_helper.get_action_url("edit", variant.pk)
         send = OutgoingEmail.objects.send(
             template_name="product_request",
             subject="Złożono zapytanie ofertowe",
             recipient=variant.template.author.email,
-            context={"product": variant},
+            context={"product": variant, "admin_url": admin_url},
             sender=settings.DEFAULT_FROM_EMAIL
         )
     except Exception as e:
