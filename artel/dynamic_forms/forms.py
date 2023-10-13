b149b85f53a4df0599c95e510f0804c68fbdd974
mtyton <mateusz.tyton99@gmail.com>
2023-10-13 20:58:49 +0200

    Basic forms has been implemented

--- artel/dynamic_forms/forms.py
+++ artel/dynamic_forms/forms.py
@@ -1,13 +1,28 @@
-from collections.abc import Mapping
-from typing import Any
 from django import forms
-from django.forms.utils import ErrorList
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
 
 
 class DynamicForm(forms.Form):
 
     FIELD_TYPE_MAPPING = {
-        "singleline": forms.CharField(max_length=255, widget=forms.TextInput(attrs={"class": "form-control"})),
+        "singleline": forms.CharField(max_length=50, widget=forms.TextInput(attrs={"class": "form-control"})),
         "multiline": forms.CharField(max_length=255, widget=forms.Textarea(attrs={"class": "form-control"})),
         "email": forms.EmailField(max_length=255, widget=forms.EmailInput(attrs={"class": "form-control"})),
         "number": forms.IntegerField(widget=forms.NumberInput(attrs={"class": "form-control"})),
@@ -17,9 +32,17 @@ class DynamicForm(forms.Form):
         "dropdown": forms.ChoiceField(widget=forms.Select(attrs={"class": "form-control"})),
     }
 
-    def __init__(self, field_list, *args, **kwargs) -> None:
+    def __init__(self, *args, **kwargs) -> None:
         self.page = kwargs.pop("page")
         self.user = kwargs.pop("user")
+        field_list = kwargs.pop("field_list")
+        file_uploads = kwargs.pop("file_uploads", False)
         super().__init__(*args, **kwargs)
         for field in field_list:
             self.fields[field.clean_name] = self.FIELD_TYPE_MAPPING[field.field_type]
+        if file_uploads:
+            self.fields["attachments"] = MultipleFileField(
+                required=True, widget=MultipleFileInput(
+                    attrs={"class": "form-control"}
+                )
+            )
--- artel/dynamic_forms/models.py
+++ artel/dynamic_forms/models.py
@@ -37,6 +37,7 @@ class Form(FormMixin, Page):
             ]),
             FieldPanel('subject'),
         ], "Email"),
+        FieldPanel("allow_attachments")
     ]
 
     def get_form_class(self):
@@ -46,9 +47,9 @@ class Form(FormMixin, Page):
         form_class = self.get_form_class()
         form_params = self.get_form_parameters()
         form_params.update(kwargs)
-        print(form_params)
-        fields = self.get_form_fields()
-        return form_class(field_list=fields, *args, **form_params)
+        form_params["field_list"] = self.get_form_fields()
+        form_params["file_uploads"] = self.allow_attachments
+        return form_class(*args, **form_params)
 
     class Meta:
         abstract = True
@@ -67,18 +68,21 @@ class EmailFormSubmission(AbstractFormSubmission):
             elif isinstance(value, datetime.date):
                 value = date_format(value, settings.SHORT_DATE_FORMAT)
 
-            content.append("{}: {}".format(field_name.label, value))
+            content.append("{}: {}".format(field_name, value))
 
         return "\n".join(content)
     
     def send_mail(self, data):
-        addresses = [x.strip() for x in data["to_address"].split(",")]
+        to_addresses = data.pop("to_address").split(",")
+        subject = data.pop("subject")
+        from_address = data.pop("from_address", settings.DEFAULT_FROM_EMAIL)
+        addresses = [x.strip() for x in to_addresses]
         send_mail(
             addresses,
             [],
-            data.pop("subject"),
+            subject,
             self.render_email(data),
-            data.pop("from_address", settings.DEFAULT_FROM_EMAIL)
+            from_address
         )
 
 
@@ -98,6 +102,22 @@ class CustomEmailForm(Form):
 
     template = "forms/email_form_page.html"
 
+    def get_submission_class(self):
+        return EmailFormSubmission
+
+    def process_form_submission(self, form):
+        submission = self.get_submission_class().objects.create(
+            form_data=form.cleaned_data,
+            page=self,
+        )
+        mail_data = form.cleaned_data.copy()
+        mail_data.update({
+            "from_address": self.from_address,
+            "to_address": self.to_address,
+            "subject": self.subject
+        })
+        submission.send_mail(data=mail_data)
+        return submission
 
 class EmailFormField(AbstractFormField):
     form = ParentalKey(
